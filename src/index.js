8f3df03146375146d6edd05e6119bd00318d66ef
nicolaspap3 <n.papachristodoulou@yahoo.com>
2025-03-18 07:37:17 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import authRoutes from "./routes/authRoutes.js"
+import bookRoutes from "./routes/bookRoutes.js"
+import { connectDB } from "./lib/db.js";
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+app.use(express.json())
+app.use(cors())
+app.use("/api/auth", authRoutes)
+app.use("/api/auth", bookRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${3000}`)
+  connectDB()
+})
