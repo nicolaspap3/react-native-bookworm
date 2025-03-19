37604c371b5de43220c87ff65ea17be950d9ef22
nicolaspap3 <n.papachristodoulou@yahoo.com>
2025-03-19 04:33:56 +0200

    cron added

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import "dotenv/config"
 import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js"
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js"
 
 const app = express()
 const PORT = process.env.PORT || 3000;
+job.start()
 app.use(express.json())
 app.use(cors())
 app.use("/api/auth", authRoutes)
---
+++ src/lib/cron.js
@@ -0,0 +1,16 @@
+import cron from "cron";
+import https from "https"
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+  https.get(process.env.API_URL, (res) => {
+    if (res.statusCode === 2000) console.log("GET request sent successfully");
+    else console.log("GET request failed", res.statusCode)
+  })
+    .on("error", e => console.error("Error while sending request", e))
+})
+
+export default job;
+
+//MINUTE,HOR,DAY OF MONTH,MONTH,DAY OF THE WEEK
+
+//* 30 3 15 * * - At 3:30am, on the 15th every month
