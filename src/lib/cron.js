9dd399e5e23c40ad1a98a7a434ec6b3e643561e6
nicolaspap3 <n.papachristodoulou@yahoo.com>
2025-03-19 05:58:52 +0200

    cron added3

--- src/lib/cron.js
+++ src/lib/cron.js
@@ -4,7 +4,7 @@ import "dotenv/config"
 
 const job = new cron.CronJob("*/14 * * * *", function () {
   https.get(process.env.API_URL, (res) => {
-    if (res.statusCode === 2000) console.log("GET request sent successfully");
+    if (res.statusCode === 200) console.log("GET request sent successfully");
     else console.log("GET request failed", res.statusCode)
   })
     .on("error", e => console.error("Error while sending request", e))
