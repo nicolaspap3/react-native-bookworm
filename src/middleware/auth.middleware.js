02d4c9744e3358b9a1fa57407d51f1f3267f5ee8
nicolaspap3 <n.papachristodoulou@yahoo.com>
2025-03-19 09:59:49 +0200

    fix3

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -8,9 +8,9 @@ const protectRoute = async (req, res, next) => {
     const token = req.header("Authorization").replace("Bearer ", "")
     if (!token) return res.status(401).json({ message: "No authentication token, access denied" })
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-    const user = await User.findById(decoded.userId).select("-password")
+    const user = await User.findById(decoded.userId).select("password")
     if (!user) return res.status(401).json({ message: "Token is not valid" })
-    req.user = user;
+    req.user = user._id;
     next()
   } catch (error) {
     console.error("Authentication error:", error.message)
