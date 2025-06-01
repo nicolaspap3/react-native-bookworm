a44ec54cfdb008628961d40bc0eb5c97e64a1b6c
nicolaspap3 <n.papachristodoulou@yahoo.com>
2025-06-01 18:23:17 +0300

    final fix2

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import express from "express"
-import cors from "cors"
-import "dotenv/config"
-import authRoutes from "./routes/authRoutes.js"
-import bookRoutes from "./routes/bookRoutes.js"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
-import bodyParser from "body-parser"
-import job from "./lib/cron.js"
+import bodyParser from "body-parser";
+import job from "./lib/cron.js";
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(bodyParser.json({ limit: '50mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
-job.start()
-app.use(cors({ origin: "*" }))
-app.use(express.json())
-app.use("/api/auth", authRoutes)
-app.use("/api/books", bookRoutes)
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+job.start();
+app.use(cors());
+// app.use(express.json())
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${3000}`)
-  connectDB()
-})
+  console.log(`Server is running on port ${3000}`);
+  connectDB();
+});
