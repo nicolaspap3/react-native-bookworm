27223b8039462b8237fe5d917991b9edc9b21a0c
nicolaspap3 <n.papachristodoulou@yahoo.com>
2025-06-03 00:07:24 +0300

    final

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ import job from "./lib/cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 job.start();
-app.use(cors());
+app.use(cors("*"));
 // app.use(express.json())
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
